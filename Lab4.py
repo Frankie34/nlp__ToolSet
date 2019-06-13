bfaa0a05069afcbed06d7db7efaa6071ed674451
Juncheng Zhu <zhujuncheng1021@163.com>
2019-06-13 23:07:16 +0800

    Lab4

---
+++ Lab4.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 13:43:51 2019
+
+@author: junchengzhu
+"""
+'''
+import nltk
+from nltk.corpus import brown
+
+def pos_features(sentence, i, history):
+     features = {"suffix(1)": sentence[i][-1:],
+                 "suffix(2)": sentence[i][-2:],
+                 "suffix(3)": sentence[i][-3:]}
+     if i == 0:
+         features["prev-word"] = "<START>"
+         features["prev-tag"] = "<START>"
+     else:
+         features["prev-word"] = sentence[i-1]
+         features["prev-tag"] = history[i-1]
+     return features
+
+class ConsecutivePosTagger(nltk.TaggerI):
+
+    def __init__(self, train_sents):
+        train_set = []
+        for tagged_sent in train_sents:
+            untagged_sent = nltk.tag.untag(tagged_sent)
+            history = []
+            for i, (word, tag) in enumerate(tagged_sent):
+                featureset = pos_features(untagged_sent, i, history)
+                train_set.append( (featureset, tag) )
+                history.append(tag)
+        self.classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+    def tag(self, sentence):
+        history = []
+        for i, word in enumerate(sentence):
+            featureset = pos_features(sentence, i, history)
+            tag = self.classifier.classify(featureset)
+            history.append(tag)
+        return zip(sentence, history)
+
+
+tagged_sents = brown.tagged_sents(categories='news')
+size = int(len(tagged_sents) * 0.1)
+train_sents, test_sents = tagged_sents[size:], tagged_sents[:size]
+tagger = ConsecutivePosTagger(train_sents)
+print(tagger.evaluate(test_sents))
+'''
+
+import nltk
+from nltk.corpus import movie_reviews
+
+documents = [(list(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+
+all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features = list(all_words)[:2000]
+
+def document_features(document): 
+    document_words = set(document) 
+    features = {}
+    for word in word_features:
+        features['contains({})'.format(word)] = (word in document_words)
+    return features
+
+print(document_features(movie_reviews.words('pos/cv957_8737.txt')))
+
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+
+print(nltk.classify.accuracy(classifier, test_set)) 
+print(classifier.show_most_informative_features(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
